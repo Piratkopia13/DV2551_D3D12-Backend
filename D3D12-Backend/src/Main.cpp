91101d6794e77e8518ce979f2845381a62ea6c1d
Alexander Wester <alexander.wester@gmail.com>
2019-01-21 20:42:32 +0100

    Added required d3d12 links (should be moved to another file later) Added SSE2 support and fast float precision

--- D3D12-Backend/src/Main.cpp
+++ D3D12-Backend/src/Main.cpp
@@ -1,5 +1,10 @@
 #include "pch.h"
 
+// Link necessary d3d12 libraries.
+#pragma comment(lib,"d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+#pragma comment(lib, "dxgi.lib")
+
 int main() {
 	std::cout << "Oh hai" << std::endl;
 	std::cin.get();
