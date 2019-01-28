f2c47c47c8d19e4a33aa7d3767419373495965df
Fredrik Junede <ettafredrik@gmail.com>
2019-01-28 17:12:49 +0100

    Temp commit for updated remote

--- D3D12-Backend/src/DX12/DX12VertexBuffer.cpp
+++ D3D12-Backend/src/DX12/DX12VertexBuffer.cpp
@@ -1,6 +1,6 @@
 #include "DX12VertexBuffer.h"
 
-DX12VertexBuffer::DX12VertexBuffer() {
+DX12VertexBuffer::DX12VertexBuffer(size_t size, ID3D12Device4) {
 }
 
 DX12VertexBuffer::~DX12VertexBuffer() {
@@ -8,7 +8,29 @@ DX12VertexBuffer::~DX12VertexBuffer() {
 }
 
 void DX12VertexBuffer::setData(const void * data, size_t size, size_t offset) {
-	
+	// Heap properties of the resource
+	D3D12_HEAP_PROPERTIES hp = {};
+	// Default heap, no CPU access, usually populated through upload heaps
+	hp.Type = D3D12_HEAP_TYPE_DEFAULT;
+	hp.CreationNodeMask = 1;
+	hp.VisibleNodeMask = 1;
+
+	// Resource description
+	D3D12_RESOURCE_DESC rd = {};
+	// Declare that the resource is used as a buffer
+	rd.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	rd.Width = size;
+	rd.Height = 1;
+	rd.DepthOrArraySize = 1;
+	rd.MipLevels = 1;
+	rd.SampleDesc.Count = 1;
+	rd.Layout = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+
+	void* dataBegin = nullptr;
+	D3D12_RANGE range = { 0, 0 }; // No intention of reading this resource on the CPU.
+	m_vertexBuffer->Map(0, &range, &dataBegin);
+	memcpy_s(dataBegin, size, data, size);
+	m_vertexBuffer->Unmap(0, nullptr);
 }
 
 void DX12VertexBuffer::bind(size_t offset, size_t size, unsigned int location) {
--- D3D12-Backend/src/DX12/DX12VertexBuffer.h
+++ D3D12-Backend/src/DX12/DX12VertexBuffer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "../VertexBuffer.h"
+#include "DX12.h"
 
 class DX12VertexBuffer : public VertexBuffer
 {
@@ -8,12 +9,12 @@ private:
 	//cheap ref counting
 	unsigned int refs = 0;
 
-	Microsoft::WRL::ComPtr<ID3D12Resource> m_vertexBuffer;
+	wComPtr<ID3D12Resource> m_vertexBuffer;
 
 public:
 	enum DATA_USAGE { STATIC=0, DYNAMIC=1, DONTCARE=2 };
 
-	DX12VertexBuffer();
+	DX12VertexBuffer(size_t size);
 	virtual ~DX12VertexBuffer();
 	virtual void setData(const void* data, size_t size, size_t offset);
 	virtual void bind(size_t offset, size_t size, unsigned int location);
