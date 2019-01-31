3765bb5da9f34e843107638dddd47f3185fae22a
Alexander Wester <alexander.wester@gmail.com>
2019-01-31 20:00:04 +0100

    fixed compile error

--- D3D12-Backend/src/Main.cpp
+++ D3D12-Backend/src/Main.cpp
@@ -266,12 +266,12 @@ int initialiseTestbench()
 	//// create texture
 	Texture2D* fatboy = renderer->makeTexture2D();
 	fatboy->loadFromFile("../assets/textures/fatboy.png");
-	Sampler2D* sampler = renderer->makeSampler2D();
+	/*Sampler2D* sampler = renderer->makeSampler2D();
 	sampler->setWrap(WRAPPING::REPEAT, WRAPPING::REPEAT);
-	fatboy->sampler = sampler;
+	fatboy->sampler = sampler;*/
 
 	textures.push_back(fatboy);
-	samplers.push_back(sampler);
+	//samplers.push_back(sampler);
 
 	//// pre-allocate one single vertex buffer for ALL triangles
 	pos = renderer->makeVertexBuffer(TOTAL_TRIS * sizeof(triPos), VertexBuffer::DATA_USAGE::STATIC);
